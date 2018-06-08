cdb29e876c3003e8b96b9a3cdedcad24bd149bd1
Anand Selvaraj <sanand.net@gmail.com>
2018-06-08 13:25:04 -0500

    SSO integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { LoginService } from './login/login.service';
+import { routes } from './app.routing'
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthGuardService } from './login/auth-guard.service';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { CredInterceptor } from './shared/cred-interceptor';
+import { QueueComponent } from './review/queue/queue.component';
+import { CertificationComponent } from './review/certification/certification.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    QueueComponent,
+    CertificationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: 
+  [LoginService, 
+   AuthGuardService,
+   {
+    provide: HTTP_INTERCEPTORS,
+    useClass: CredInterceptor,
+    multi: true
+  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/cred-interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/observable/throw'
+import 'rxjs/add/operator/catch';
+import { environment } from '../../environments/environment';
+
+const clientId = environment.clientId;
+const clientSecret = environment.clientSecret;
+
+@Injectable()
+export class CredInterceptor implements HttpInterceptor {
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        
+        let headers = new HttpHeaders();
+        headers = headers.append("Content-type", "application/json");
+        headers = headers.append("Authorization", "Basic " + btoa(clientId+":"+clientSecret));
+
+        const authReq = req.clone({ headers: headers });
+
+        return next.handle(authReq)
+            .catch((error, caught) => {
+                console.log(error);
+                return Observable.throw(error);
+            }) as any;
+    }
+}
